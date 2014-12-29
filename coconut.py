047443a936f7357437c0de16eebfc1e7ac1b5d0f
Theodore Randolph <trandolp@gmail.com>
2014-12-29 15:31:00 -0800

    Slight miswording in another comment. Doesn't affect the validity of the code, either.

--- coconut.py
+++ coconut.py
@@ -14,7 +14,7 @@ Algorithmic approach: Use 2 data structures.
   2 is the jet streams, sorted by starting position, ascending.
 For each jet stream, cycle through the open air slots, separating them into 2 groups.
   One group is valid to add to them. Find the lowest cost of these, and add to it. Keep the lowest cost and the added.
-    I know that any later jet stream under consideration will start later than this one, so higher than the lowest cost is higher for those, too.
+    I know that any later jet stream under consideration will start at least as late as this one, so higher than the lowest cost is higher for those, too.
   One group is not valid to add to them. Keep these, too.
 At the end, take the open air slots, and find the lowest distance of these.
 
