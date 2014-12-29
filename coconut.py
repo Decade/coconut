ffa474f15c20ee224ffc2bfece94f3acbea0928f
Theodore Randolph <trandolp@gmail.com>
2014-12-29 14:23:53 -0800

    More documentation
    Now with comments, mostly in the form of docstrings.
    
    Also change the names of a couple of the variables in the list comprehensions, to make them clearer.

--- coconut.py
+++ coconut.py
@@ -1,5 +1,6 @@
-import sys
+
 """
+Transporting a coconut.
 Design issue:
 There are 2 types of movement:
   Normal movement. No jet stream. Normal movement can be interrupted at any time.
@@ -16,18 +17,56 @@ For each jet stream, cycle through the open air slots, separating them into 2 gr
     I know that any later jet stream under consideration will start later than this one, so higher than the lowest cost is higher for those, too.
   One group is not valid to add to them. Keep these, too.
 At the end, take the open air slots, and find the lowest distance of these.
+
+Going to use a functional approach: Do not mutate a data structure once it has finished being built, and build it in a very simple manner.
 """
 
+import sys
+
+
 class Jetstream:
+    """Jetstream class to hold data and representation of a jetstream.
+
+    Note: Start and end as in normal Unix convention. The end of one Jetstream may be the start of the next.
+
+    Static objects:
+    
+    start -- Starting point
+    end -- Where the bird ends up
+    cost -- Cost of riding this jetstream
+    """
     def __init__(self, start, end, cost):
         self.start = start
         self.end = end
         self.cost = cost
-    def __unicode__(self):
+    def __str__(self):
         return '(' + str(self.start) + ',' + str(self.end) + ')'
 
 class Path:
+    """Path class. To hold a path.
+
+    Static objects:
+
+    weight -- Open-air cost of flying
+    open -- Where the open air starts again
+    cost -- How much energy it takes to get to this open point
+    jetstreams -- A list of jetstreams in this path.
+
+    Functions:
+
+    pathcost() -- How much it would take to go a certain distance past the open point using this path
+    invalid() -- Whether the pathcost calculation would make sense for this certain distance
+
+    """
     def __init__(self,*,weight=1,predecessor=None,jetstream=None):
+        """__init__()
+        
+        Two ways to construct: New path, and adding a jetstream to a path.
+        Path(weight = [n]) -- A 0-jetstream path with a certain open-air weight
+        Path(predecessor = [pathlike object], jetstream = [new stream]) -- A new path with the previous path's jetstreams, adding a new jetstream
+
+        It's the responsibility of the caller to check that the new jetstream makes sense to add.
+        """
         if predecessor == None or jetstream == None:
             self.weight = weight
             self.open = 0
@@ -39,24 +78,54 @@ class Path:
             self.cost = predecessor.cost + jetstream.cost + self.weight * (jetstream.start - predecessor.open)
             self.jetstreams = predecessor.jetstreams + [jetstream]
     def pathcost(self,position):
+        """pathcost(position)
+
+        It's the responsibility of the coller to check that the position makes sense to calculate.
+        """
         return self.cost + self.weight * (position - self.open)
     def isvalid(self,position):
+        """invalid(position)
+
+        Validity is defined as being in open air at that position.
+        """
         return position >= self.open
-    def __unicode__(self):
-        return '[' + ', '.join(i.__unicode__() for i in self.jetstreams) + ']'
+    def __str__(self):
+        return '[' + ', '.join(str(i) for i in self.jetstreams) + ']'
 
 def minimumpath(paths, position):
+    """minimumpath(paths, position)
+
+    Takes an iterable of paths and returns the path with the lowest cost for the position.
+    Prerequisite: All of the paths are actually valid for that position, otherwise the calculation is wrong.
+    """
     return min(paths,key=lambda x: x.pathcost(position))
 
 def finalminimum(paths):
-    finaldest = max(paths,key=lambda x: x.open)
+    """fininalminimum(paths)
+
+    Takes an iterable of paths and returns a tuple of the lowest-cost path for the longest distance tuple in that path, and the final cost over that path.
+    """
+    finaldest = max(paths,key=lambda path: path.open)
     minimum = minimumpath(paths,finaldest.open)
     return minimum,minimum.pathcost(finaldest.open)
 
 def calcpath(paths,streams):
+    """calcpath(paths, streams)
+
+    Takes an initial iterable of paths and a sorted iterable of jetstreams to add, and returns whatever finalminimum() returns.
+    Which is a tuple of the lowest-cost path and the final cost over that path.
+
+    Function signature and operation inspired by recursive Scheme style, but turned into an iteration because Python is more efficient with iteration than recursion.
+    And this problem is small enough that I don't need to make it execute in parallel, or reuse any data structures.
+
+    Normal way to call this:
+    calcpath([Path(weight=[n])],jetstreams)
+
+    Function arguments are not modified.
+    """
     for jet in streams:
-        valids = []
-        laters = []
+        valids = [] # Paths that are valid to add a jetstream to.
+        laters = [] # Paths that are kept for the next streams.
         for path in paths:
             if path.isvalid(jet.start):
                 valids.append(path)
@@ -70,15 +139,19 @@ def calcpath(paths,streams):
 
 def main():
     if len(sys.argv) != 2:
+        print("Usage: " + sys.argv[0] + " paths.txt")
         sys.exit(1)
+    # Just going to act like this file is here and non-malicious, etc.
     file = open(sys.argv[1])
     weight = int(file.readline())
-    jets = sorted((Jetstream(int(i[0]),int(i[1]),int(i[2])) for i in (i.split() for i in file.readlines())),key = lambda x: x.start)
+    # Neato unreadable functional programming. Create a sorted list of jetstreams by
+    # reading lines, splitting the lines into words, and parsing the words as ints for the Jetstream constructor.
+    jets = sorted((Jetstream(int(i[0]),int(i[1]),int(i[2])) for i in (line.split() for line in file.readlines())),key = lambda stream: stream.start)
     file.close()
     seed = Path(weight=weight)
     path, cost = calcpath([seed],jets)
     print(cost)
-    print(path.__unicode__())
+    print(path)
 
 if __name__ == '__main__':
     main()
